10aeee41dff10275bd9404f7866166d861dbae09
Justin Boyson <unclejustin@gmail.com>
2022-10-24 12:58:25 -0500

    format

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,11 +14,13 @@ export default function Index() {
 					<a
 						href="https://github.com/remix-stacks/playwright"
 						target="_blank"
-						rel="noopener noreferrer">
+						rel="noopener noreferrer"
+					>
 						<svg
 							className="h-8 w-8 fill-gray-700 transition hover:scale-110 hover:opacity-60 active:scale-100"
 							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24">
+							viewBox="0 0 24 24"
+						>
 							<path
 								fillRule="evenodd"
 								clipRule="evenodd"
@@ -31,11 +33,13 @@ export default function Index() {
 					<a
 						href="https://twitter.com/DanielKanem"
 						target="_blank"
-						rel="noopener noreferrer">
+						rel="noopener noreferrer"
+					>
 						<svg
 							className="h-8 w-8 fill-gray-700 transition hover:scale-110 hover:opacity-60 active:scale-100"
 							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg">
+							xmlns="http://www.w3.org/2000/svg"
+						>
 							<path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z" />
 						</svg>
 					</a>
@@ -58,7 +62,8 @@ export default function Index() {
 						<div className="mb-1" />
 						<p
 							className="cursor-default text-base font-semibold text-gray-900
-            	drop-shadow-sm hover:opacity-50">
+            	drop-shadow-sm hover:opacity-50"
+						>
 							Open Source Remix Template
 						</p>
 					</div>
@@ -142,7 +147,8 @@ export default function Index() {
 							target="_blank"
 							rel="noopener noreferrer"
 							className="flex h-14 w-28 select-none justify-center p-1 drop-shadow-sm
-              grayscale transition hover:scale-110 hover:grayscale-0 focus:grayscale-0">
+              grayscale transition hover:scale-110 hover:grayscale-0 focus:grayscale-0"
+						>
 							<img alt={img.alt} src={img.src} />
 						</a>
 					))}
@@ -153,7 +159,8 @@ export default function Index() {
 			<footer className="z-10 flex flex-col items-center p-8">
 				<div
 					className="fixed bottom-4 flex select-none flex-row items-center justify-between
-					rounded-full bg-white p-3 px-6 shadow-2xl transition hover:scale-105">
+					rounded-full bg-white p-3 px-6 shadow-2xl transition hover:scale-105"
+				>
 					{/* Socials. */}
 					<div className="flex flex-row items-center">
 						<div className="flex flex-col">
@@ -167,12 +174,14 @@ export default function Index() {
 							<a
 								href="https://github.com/remix-run"
 								target="_blank"
-								rel="noopener noreferrer">
+								rel="noopener noreferrer"
+							>
 								<svg
 									className="h-6 w-6 fill-gray-400 transition hover:scale-110
 									hover:fill-gray-900 active:scale-100"
 									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24">
+									viewBox="0 0 24 24"
+								>
 									<path
 										fillRule="evenodd"
 										clipRule="evenodd"
@@ -185,12 +194,14 @@ export default function Index() {
 							<a
 								href="https://twitter.com/remix_run"
 								target="_blank"
-								rel="noopener noreferrer">
+								rel="noopener noreferrer"
+							>
 								<svg
 									className="h-6 w-6 fill-gray-400 transition hover:scale-110
 									hover:fill-gray-900 active:scale-100"
 									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg">
+									xmlns="http://www.w3.org/2000/svg"
+								>
 									<path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z" />
 								</svg>
 							</a>
@@ -199,12 +210,14 @@ export default function Index() {
 							<a
 								href="https://discord.com/invite/xwx7mMzVkA"
 								target="_blank"
-								rel="noopener noreferrer">
+								rel="noopener noreferrer"
+							>
 								<svg
 									className="h-6 w-6 fill-gray-400 transition hover:scale-110
 									hover:fill-gray-900 active:scale-100"
 									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg">
+									xmlns="http://www.w3.org/2000/svg"
+								>
 									<path d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z" />
 								</svg>
 							</a>
