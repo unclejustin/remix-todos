00e5383b98c2d2212ab404feb060414c53e0b26d
Justin Boyson <unclejustin@gmail.com>
2022-10-26 22:10:31 -0500

    run migrate and seed before e2e

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,21 @@ const prisma = new PrismaClient()
 async function seed() {
 	// Cleanup existing database.
 	await prisma.welcome.deleteMany({})
+	await prisma.todo.deleteMany({})
 
 	// Create seed.
 	await prisma.welcome.create({
 		data: {
 			message: '😳 If you see this message, support us with a star on Github!',
 		},
+	})
+
+	await prisma.todo.create({
+		data: { todo: 'Complete item', completed: true },
+	})
+
+	await prisma.todo.create({
+		data: { todo: 'Incomplete item', completed: false },
 	})
 
 	console.log(`Database has been successfully seeded. 🌱`)
